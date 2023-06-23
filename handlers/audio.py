d7596692e9ffc50ecc13e07bd8f58d7b6f44c1db
Hiro <laciferin@gmail.com>
2023-06-23 08:34:00 +0530

    fixed audio
    Signed-off-by: Hiro <laciferin@gmail.com>

--- handlers/audio.py
+++ handlers/audio.py
@@ -1,3 +1,4 @@
+import base64
 from pathlib import Path
 
 from typing import IO
@@ -7,11 +8,27 @@ from utils.outputs.text_to_speech import text_to_speech, TextToSpeechConfig
 import streamlit as st
 
 
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+
 def handle_text_2_speech(text: str | Path | IO):
     if not text:
         st.error(f"invalid input:{type(text)}")
         return
 
-    config = TextToSpeechConfig(text=text, output=io.BytesIO())
+    config = TextToSpeechConfig(text=text, output="output.mp3")
     text_to_speech(config)
-    st.audio(config.output, config.format, start_time=0)
+    # st.audio(config.output, config.format, start_time=0)
+    autoplay_audio(config.output)
