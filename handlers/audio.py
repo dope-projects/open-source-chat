0c3281d4caddccbe899eacd582522191d9356bf3
Hiro <laciferin@gmail.com>
2023-06-23 08:52:48 +0530

    feat: audio
    Signed-off-by: Hiro <laciferin@gmail.com>

--- handlers/audio.py
+++ handlers/audio.py
@@ -8,19 +8,26 @@ from utils.outputs.text_to_speech import text_to_speech, TextToSpeechConfig
 import streamlit as st
 
 
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        md = f"""
-            <audio controls autoplay="true">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-            </audio>
-            """
-        st.markdown(
-            md,
-            unsafe_allow_html=True,
-        )
+def autoplay_audio(audio: str | io.BytesIO):
+    if isinstance(audio, str):
+        with open(audio, "rb") as f:
+            data = f.read()
+    elif isinstance(audio, io.BytesIO):
+        data = audio.getvalue()
+        audio.seek(0)
+    else:
+        raise ValueError("Invalid audio input type.")
+
+    b64 = base64.b64encode(data).decode()
+    md = f"""
+        <audio controls autoplay="true">
+        <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+        </audio>
+        """
+    st.markdown(
+        md,
+        unsafe_allow_html=True,
+    )
 
 
 def handle_text_2_speech(text: str | Path | IO):
@@ -28,7 +35,7 @@ def handle_text_2_speech(text: str | Path | IO):
         st.error(f"invalid input:{type(text)}")
         return
 
-    config = TextToSpeechConfig(text=text, output="output.mp3")
+    config = TextToSpeechConfig(text=text, output=io.BytesIO())
     text_to_speech(config)
     # st.audio(config.output, config.format, start_time=0)
     autoplay_audio(config.output)
--- utils/outputs/text_to_speech.py
+++ utils/outputs/text_to_speech.py
@@ -11,7 +11,7 @@ class TextToSpeechConfig:
     output: str | io.BytesIO | Path
     tld: str = 'com'
     lang: str = "en"
-    slow: bool = True
+    slow: bool = False
     format: str = "audio/mp3"
 
 
@@ -20,4 +20,11 @@ def text_to_speech(config: TextToSpeechConfig):
                lang=config.lang,
                slow=config.slow,
                tld=config.tld, )
-    tts.save(config.output)
+
+    out = config.output
+    if isinstance(out, str) or isinstance(out, Path):
+        tts.save(out)
+    elif isinstance(out, io.BytesIO):
+        tts.write_to_fp(out)
+    else:
+        raise ValueError("invalid format")
