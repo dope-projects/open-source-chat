d8bd9b1d866b851aefaf2dabe83d550636683df9
Hiro <laciferin@gmail.com>
2023-06-23 07:58:41 +0530

    wip: audio
    Signed-off-by: Hiro <laciferin@gmail.com>

--- handlers/audio.py
+++ handlers/audio.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 
 from typing import IO
+import io
 
-from utils.outputs.text_to_speech import text_to_speech
+from utils.outputs.text_to_speech import text_to_speech, TextToSpeechConfig
 import streamlit as st
 
 
-def handle_text_2_speech(input_text: str | Path | IO):
-    if input_text:
-        output_file = text_to_speech(input_text)
-        st.audio(output_file, format='audio/mp3')
+def handle_text_2_speech(text: str | Path | IO):
+    if not text:
+        st.error("invalid input")
+        return
+
+    config = TextToSpeechConfig(text=text, output=io.BytesIO())
+    output_file = text_to_speech(config)
+    st.audio(output_file, format='audio/mp3')
