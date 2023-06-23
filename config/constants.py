cdaf64bbf181bd5cc89ae59acd337e5ead18c4fe
Hiro <laciferin@gmail.com>
2023-06-23 05:48:00 +0530

    add: modes
    Signed-off-by: Hiro <laciferin@gmail.com>

--- config/constants.py
+++ config/constants.py
@@ -7,7 +7,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+PRODUCTION: Final[str] = "production"
+DEVELOPMENT: Final[str] = "development"
+TESTING: Final[str] = "testing"
+
 PINECONE_API_KEY: Final[str] = environ["PINECONE_API_KEY"]
 PINECONE_API_ENV: Final[str] = environ["PINECONE_API_ENV"]
 OPENAI_API_KEY: Final[str] = environ["OPENAI_API_KEY"]
 INDEX_NAME: Final[str] = os.getenv("INDEX_NAME", "chatgpt")
+MODE: Final[str] = os.getenv("mode", DEVELOPMENT)
